97d548cf5b1678b28d1d49bf5ea9a1424b77e7d9
santipu <santipujolsau@gmail.com>
2022-12-26 20:09:48 +0100

    created functions to store the contract in an array and to retrieve it to the frontend

--- server/index.js
+++ server/index.js
@@ -6,35 +6,17 @@ const port = 3042
 app.use(cors())
 app.use(express.json())
 
-// app.get("/set", (req, res) => {
-//     const { address } = req.params
-//     const balance = balances[address] || 0
-//     res.send({ balance })
-// })
+const contracts = []
 
-// app.post("/send", (req, res) => {
-//     const { amount, recipient, msgHash, signature, recoveryBit } = req.body
-//     let recoveredPublicKey, senderAddress
+app.post("/storeContract", (req, res) => {
+    const { contract } = req.body
+    console.log(contract)
+    contracts.push(contract)
+})
 
-//     recoverPublicKey(msgHash, signature, recoveryBit).then((publicKey) => {
-//         recoveredPublicKey = publicKey
-//         senderAddress = `0x${toHex(keccak256(recoveredPublicKey).slice(1).slice(-20))}`
-
-//         setInitialBalance(senderAddress)
-//         setInitialBalance(recipient)
-
-//         if (balances[senderAddress] < amount) {
-//             res.status(400).send({ message: "Not enough funds!" })
-//         } else if (usedSignatures.includes(signature)) {
-//             res.status(400).send({ message: "Signature already used!" })
-//         } else {
-//             usedSignatures.push(signature)
-//             balances[senderAddress] -= amount
-//             balances[recipient] += amount
-//             res.send({ balance: balances[senderAddress], senderAddress })
-//         }
-//     })
-// })
+app.get("/getContracts", (req, res) => {
+    res.send({ contracts })
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}!`)
