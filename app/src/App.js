6f26fbd1033d335057143262401aaba1c2fd99d2
santipu <santipujolsau@gmail.com>
2022-12-26 12:04:07 +0100

    changed input value from wei to ether

--- app/src/App.js
+++ app/src/App.js
@@ -29,7 +29,9 @@ function App() {
     async function newContract() {
         const beneficiary = document.getElementById("beneficiary").value
         const arbiter = document.getElementById("arbiter").value
-        const value = ethers.BigNumber.from(document.getElementById("wei").value)
+        const value = ethers.BigNumber.from(
+            (document.getElementById("wei").value * 10 ** 18).toString()
+        )
         const escrowContract = await deploy(signer, arbiter, beneficiary, value)
 
         const escrow = {
@@ -68,7 +70,7 @@ function App() {
                     </label>
 
                     <label>
-                        Deposit Amount (in Wei)
+                        Deposit Amount (in ETH)
                         <input type="text" id="wei" />
                     </label>
 
